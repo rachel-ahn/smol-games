bfa21e0cba90a8ec9bdc43222b56aa4ed54282cd
rachel <rachelyk11@gmail.com>
2023-11-21 12:38:56 +1100

    start

---
+++ tictactoe/src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+import * as React from 'react';
+import { Button } from '@mui/material';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  return (
+    <>
+      {/* <div className="App"> */}
+        <header className="App-header">
+          Tic Tac Toe =w=
+        </header>
+        <div className="body">
+          <Button variant="contained">Contained</Button>
+        </div>
+      {/* </div> */}
+    </>
+  );
+}
+
+export default App;
